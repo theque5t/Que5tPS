5c29cb084656e1a4a88937cf2186c3c7c7f0d74b
theque5t <theque5t@users.noreply.github.com>
2021-11-12 18:11:21 -0600

    Added ConvertTo_ADA

--- Scripts/Cardano.ps1
+++ Scripts/Cardano.ps1
@@ -1,3 +1,25 @@
+function ConvertTo-ADA{
+    param(
+        [parameter(ValueFromPipeline)]
+        $Amount,
+        [ValidateSet('Lovelace')]
+        $From = 'Lovelace',
+        [switch]$WithSymbol = $True
+    )
+    switch ($From) {
+        'Lovelace' { 
+            if($Amount.unit -eq 'lovelace'){
+                $Amount = $Amount.where({$_.unit -eq 'lovelace'}).quantity
+            }
+            $ada = $Amount/1000000 }
+    }
+
+    if($WithSymbol){
+        $ada = "₳$ada"
+    }
+    return $ada
+}
+
 function Get-BlockfrostApiResponse {
     param(
         $Method = 'Get',
