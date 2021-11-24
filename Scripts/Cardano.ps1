8914858f52730a036d62f19d155ef352e147ae00
theque5t <theque5t@users.noreply.github.com>
2021-11-24 16:33:28 -0600

    WIP

--- Scripts/Cardano.ps1
+++ Scripts/Cardano.ps1
@@ -92,24 +92,57 @@ function Get-CardanoTransactionUtxos {
 }
 
 function Invoke-CardanoCLI {
-    &"$env:DEADALUS_MAINNET_HOME\cardano-cli.exe" @args
+    $output = &"$env:DEADALUS_MAINNET_HOME\cardano-cli.exe" @args 2>&1
+    if($LastExitCode){
+        $commandException = $output
+        throw "$commandException"
+    }
+    return $output
 }
 
 Set-Alias -Name cardano-cli -Value Invoke-CardanoCLI
 
-function Set-CardanoNodeSocketPath {
-    if(-not $env:CARDANO_NODE_SOCKET_PATH){
-    
-        $process = $(
-            Get-Process -Name cardano-node
-        ).Where({ 
-            $_.Path -eq "$env:DEADALUS_MAINNET_HOME\cardano-node.exe" 
+function Get-CardanoNodeProcess {
+    $process = $(Get-Process).Where({ 
+        $_.Name -eq 'cardano-node' 
         })
     
-        $pattern = '--socket-path\s(?<socket_path>.*?)\s-'
-        $process.CommandLine -match $pattern | Out-Null
+    return $process
+}
     
-        $env:CARDANO_NODE_SOCKET_PATH = $Matches.socket_path
+function Test-CardanoNodeIsRunning {
+    $process = Get-CardanoNodeProcess
+
+    return $process.Count -gt 0
+}
+
+function Set-CardanoNodeProcessRunning{
+    [CmdletBinding()]
+    param()
+    
+    if(-not $(Test-CardanoNodeIsRunning)){
+        Write-VerboseLog 'Starting Cardano node process...'
+        Start-Process `
+            -FilePath "$env:DEADALUS_MAINNET_HOME\cardano-launcher.exe" `
+            -WorkingDirectory $env:DEADALUS_MAINNET_HOME
+        
+        while(-not $(Test-CardanoNodeIsRunning)){
+            Write-VerboseLog 'Waiting for Cardano node to start...'
+            Start-Sleep -Seconds 5
+        }
+
+        Write-VerboseLog 'Cardano node process started'
+    }
+}
+
+function Set-CardanoNodeProcessStopped{
+    [CmdletBinding()]
+    param()
+
+    if($(Test-CardanoNodeIsRunning)){
+        Write-VerboseLog 'Stopping Cardano node process...'
+        Get-CardanoNodeProcess | Stop-Process
+        Write-VerboseLog 'Cardano node process stopped'
     }
 }
 
@@ -118,9 +151,51 @@ function Get-CardanoNodeTip {
         [ValidateSet('mainnet','testnet')]
         $Network = 'mainnet'
     )
-    Set-CardanoNodeSocketPath
+    try{
     $query = Invoke-CardanoCLI query tip --$Network
     return $($query | ConvertFrom-Json)
+}
+    catch{}
+}
+
+function Set-CardanoNodeSocketPath {
+    [CmdletBinding()]
+    param()
+    
+    if(-not $env:CARDANO_NODE_SOCKET_PATH -and $(Test-CardanoNodeIsRunning)){
+        $process = Get-CardanoNodeProcess
+        $pattern = '--socket-path\s(?<socket_path>.*?)\s-'
+        $process.CommandLine -match $pattern | Out-Null
+        $env:CARDANO_NODE_SOCKET_PATH = $Matches.socket_path
+        Write-VerboseLog "Set Cardano node socket path to $env:CARDANO_NODE_SOCKET_PATH"
+    }
+}
+
+function Open-CardanoNodeSession {
+    [CmdletBinding()]
+    param()
+    Write-VerboseLog 'Opening Cardano node session...'
+
+    Set-CardanoNodeProcessRunning
+    Set-CardanoNodeSocketPath
+
+    do{
+        Write-VerboseLog 'Waiting for Cardano node to respond...'
+        Start-Sleep -Seconds 5
+    }
+    while(-not $(Get-CardanoNodeTip))
+
+    Write-VerboseLog 'Cardano node session opened'
+}
+
+function Close-CardanoNodeSession {
+    [CmdletBinding()]
+    param()
+
+    Write-VerboseLog 'Closing Cardano node session...'
+    Set-CardanoNodeProcessStopped
+    Remove-Item Env:\CARDANO_NODE_SOCKET_PATH
+    Write-VerboseLog 'Cardano node session closed'
 }
 
 function Test-CardanoNodeInSync {
@@ -131,9 +206,11 @@ function Sync-CardanoNode {
     [CmdletBinding()]
     param()
 
+    Open-CardanoNodeSession
+    
     do{
-        Write-Verbose "$(Get-Date) Sync percentage: $($(Get-CardanoNodeTip).syncProgress)"
-        Start-Sleep -Seconds 1
+        Write-VerboseLog "Sync percentage: $($(Get-CardanoNodeTip).syncProgress)"
+        Start-Sleep -Seconds 5
     }
     while(-not $(Test-CardanoNodeInSync))
 }
