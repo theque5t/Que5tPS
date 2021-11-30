dd36ab7090d194443924a25953ef2780c9ef0970
theque5t <theque5t@users.noreply.github.com>
2021-11-30 01:57:46 -0600

    Added Assert-CommandSuccessful

--- Scripts/Base.ps1
+++ Scripts/Base.ps1
@@ -88,3 +88,8 @@ function Write-FalseAssertionError {
     Write-TerminatingError "False Assertion: $(Get-FunctionName -StackNumber 2)"
 }
 
+function Assert-CommandSuccessful{
+    if($LastExitCode){
+        Write-FalseAssertionError
+    }
+}
