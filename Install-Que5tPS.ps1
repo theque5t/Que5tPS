0764d3d9ddce91d1b148d69c439f6f609672acf5
theque5t <theque5t@users.noreply.github.com>
2021-12-11 22:15:11 -0600

    Changed re-write all the files

--- Install-Que5tPS.ps1
+++ Install-Que5tPS.ps1
@@ -1,6 +1,9 @@
 try {
     Write-Output "Installing Que5tPS..."
-    Copy-Item -Force -Recurse -Path $PSScriptRoot\* -Destination $(Split-Path -Path $profile)
+    $profileDirectory = $(Split-Path -Path $profile)
+    Get-ChildItem $profileDirectory -exclude 'Modules','Scripts' | 
+        Remove-Item -Recurse -Force
+    Copy-Item -Force -Recurse -Path $PSScriptRoot\* -Exclude '.git' -Destination $profileDirectory
     Write-Output "Install Complete"
 }
 catch {
