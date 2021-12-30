de4422fa2825d64a35b1a01d78f5b4b0cafbee38
theque5t <theque5t@users.noreply.github.com>
2021-12-30 16:30:43 -0600

    WIP

--- Que5tPS/PSCardanoWallet/Get-CardanoWalletAddressUtxos.ps1
+++ Que5tPS/PSCardanoWallet/Get-CardanoWalletAddressUtxos.ps1
@@ -23,56 +23,15 @@ function Get-CardanoWalletAddressUtxos {
         $File
     )
     process{
-        try{
-            Assert-CardanoWalletSessionIsOpen
-            Write-VerboseLog "Getting wallet address utxo..."
+        Assert-CardanoWalletSessionIsOpen
+        Write-VerboseLog "Getting wallet address utxo..."
 
-            $walletPath = $(Get-CardanoWallet $Name).FullName
-            $walletTemp = "$walletPath\.temp"
-            $outputFile = "$walletTemp\queryUtxoOutput-$($(New-Guid).Guid).json"
-            $_args = @(
-                'query','utxo'
-                '--address', $(Get-CardanoWalletAddress $Name $File)
-                '--out-file', $outputFile
-                $env:CARDANO_CLI_NETWORK_ARG
-                $env:CARDANO_CLI_NETWORK_ARG_VALUE
-            )
-            Invoke-CardanoCLI @_args
-            $queryOutput = Get-Content $outputFile | ConvertFrom-Json -AsHashtable
-            $utxos = [CardanoUtxoList]::new()
-            $queryOutput.GetEnumerator().ForEach({
-                Write-VerboseLog "Processing utxo $($_.Name)..."
-                $utxo = [CardanoUtxo]::new($_.Name, $_.Value.address, $_.Value.data)
-                $_.Value.value.GetEnumerator().ForEach({ 
-                    Write-VerboseLog "Processing value $($_.Name)..."
-                    $token = $_
-                    switch ($token.value.GetType().Name) {
-                        'Hashtable' { 
-                            $tokenPolicyId = $token.Name
-                            $token.Value.GetEnumerator().ForEach({
-                                Write-VerboseLog "Processing token $($_.Name)..."
-                                $utxo.AddToken(
-                                    $tokenPolicyId, $_.Name, $_.Value
-                                )
-                            })
-                        }
-                        'Int64' { 
-                            Write-VerboseLog "Processing token $($_.Name)..."
-                            $utxo.AddToken(
-                                $null, $token.Name, $token.Value
-                            )
-                        }
-                    }
-                })
-                $utxos.AddUtxo($utxo)
-            })
+        $walletPath = $(Get-CardanoWallet $Name).FullName
+        $walletTemp = "$walletPath\.temp"
 
-            return $utxos
-        }
-        finally{
-            if($outputFile -and $(Test-Path $outputFile)){
-                Remove-Item $outputFile
-            }
-        }
+        $utxos = Get-CardanoAddressUtxos `
+                    -Address $(Get-CardanoWalletAddress $Name $File)`
+                    -WorkingDir $walletTemp
+        return $utxos
     }
 }
---
+++ Que5tPS/PSCardanoWallet/Module.ps1
@@ -0,0 +1,1 @@
+# $PSDefaultParameterValues["Get-CardanoWalletAddressUtxos:Name"]={ if(Test-Path env:CARDANO_WALLET){ $env:CARDANO_WALLET } }
