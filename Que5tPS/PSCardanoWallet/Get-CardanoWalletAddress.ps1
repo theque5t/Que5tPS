572e50c6a150ba82e78e64378fa00261748d6a43
theque5t <theque5t@users.noreply.github.com>
2021-12-30 23:05:28 -0600

    WIP

--- Que5tPS/PSCardanoWallet/Get-CardanoWalletAddress.ps1
+++ Que5tPS/PSCardanoWallet/Get-CardanoWalletAddress.ps1
@@ -2,7 +2,10 @@ function Get-CardanoWalletAddress {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true, Position = 0)]
-        [ArgumentCompleter({ $(Get-CardanoWallets).Name })]
+        [ArgumentCompleter({ 
+            param($Command, $Param, $Input, $AST, $ParamState)
+            $(Get-CardanoWallets).Name 
+        })]
         [ValidateScript({ $_ -in $(Get-CardanoWallets).Name },
          ErrorMessage = (
          "The wallet, {0}, " + 
@@ -12,10 +15,18 @@ function Get-CardanoWalletAddress {
         [Parameter(Mandatory = $true, Position = 1)]
         [ArgumentCompleter({ 
             param($Command, $Param, $Input, $AST, $ParamState)
-            $(Get-CardanoWalletAddressFiles $ParamState.Name).Name 
+            $Name = Get-BoundValueElseDefaultValue `
+                        Name `
+                        $ParamState `
+                        $Command
+            $(Get-CardanoWalletAddressFiles $Name).Name 
         })]
         [ValidateScript({ 
-            $_ -in $(Get-CardanoWalletAddressFiles $PSBoundParameters.Name).Name 
+            $Name = Get-BoundValueElseDefaultValue `
+                        Name `
+                        $PSBoundParameters `
+                        $PSCmdlet.MyInvocation.MyCommand
+            $_ -in $(Get-CardanoWalletAddressFiles $Name).Name 
         },
          ErrorMessage = (
          "The wallet address file, {0}, " +
