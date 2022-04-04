afc0d1c6dea32e92906792535e1616512bec6770
theque5t <theque5t@users.noreply.github.com>
2022-04-03 21:31:57 -0500

    WIP

--- Que5tPS/PSCardanoNode/Get-CardanoTransactionStateHash.ps1
+++ Que5tPS/PSCardanoNode/Get-CardanoTransactionStateHash.ps1
@@ -5,7 +5,8 @@ function Get-CardanoTransactionStateHash {
         [CardanoTransaction]$Transaction
     )
     $state = Get-Content $Transaction.StateFile | 
-             Select-String -Pattern SignedStateHash -NotMatch
+             Select-String -Pattern SignedStateHash -NotMatch |
+             Select-String -Pattern Submitted -NotMatch
     $stateStream = [IO.MemoryStream]::new([Text.Encoding]::UTF8.GetBytes($state))
     $stateHash = $(Get-FileHash -InputStream $stateStream).Hash
     return $stateHash
